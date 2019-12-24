7ec73e462b0a6948adbb7c0e25adf62cc6d6f6b0
caseymacdonald213 <44450668+caseymacdonald213@users.noreply.github.com>
2019-12-24 14:44:40 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="index.html">News</a></li>
+  <li><a href="index.html">Contact</a></li>
+  <li><a href="index.html">About</a></li>
+</ul>
+
+</body>
+</html>
